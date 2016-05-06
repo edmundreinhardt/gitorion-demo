bdd503136d9ed7e9b6dd005c8dd6f97d76ab624f
Edmund Reinhardt <reinhard@ereinhardt.canlab.ibm.com>
2016-05-06 00:08:52 -0400

    refactor for bottle.py
    create show_table() to generate html into  a string
    then print that out to the console if called from the console

--- FrontEnd/showTable.py
+++ FrontEnd/showTable.py
@@ -1,102 +1,97 @@
+#!/QOpenSys/usr/bin/python3
 import sys
 import cgi
 import os
 from itoolkit import *
 from itoolkit.rest.irestcall import *
+
+def show_table():
+  # ---------------------
+  # remote call to yips
+  # ----------------------
+  itransport = iRestCall("http://65.183.160.36/cgi-bin/xmlcgi.pgm","*NONE","*NONE")
+  
+  # ---------------------
+  # normal fastcgi processing
+  # ----------------------
+  myCat = '1'
+  #myMax = form['myMax'][0]
+  myMax = '20'
+  myCount = '0'
+  findMe = ''
+  
+  #     D ARRAYMAX        c                   const(999)
+  #     D prod_t          ds                  qualified based(Template)
+  #     D  prod                         10i 0
+  #     D  cat                          10i 0
+  #     D  title                        64a   varying(4)
+  #     D  photo                        64a   varying(4)
+  #     D  price                        12p 2
+  #      *+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  #      * main(): Control flow
+  #      *+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  #     D Main            PI 
+  #     D  myCat                        10i 0
+  #     D  myMax                        10i 0
+  #     D  myCount                      10i 0
+  #     D  findMe                             likeds(prod_t) dim(ARRAYMAX)
+  itool = iToolKit()
+  itool.add(iCmd('chglibl', 'CHGLIBL LIBL(HATS) CURLIB(HATS)'))
+  itool.add(
+   iPgm('product','PRODUCT')
+   .addParm(iData('myCat','10i0',myCat))
+   .addParm(iData('myMax','10i0',myMax))
+   .addParm(iData('myCount','10i0',myCount,{'enddo':'myCount'}))
+   .addParm(
+    iDS('findMe',{'dim':'999','dou':'myCount'})
+    .addData(iData('prod','10i0',''))
+    .addData(iData('cat','10i0',''))
+    .addData(iData('title','64a','',{'varying':'4'}))
+    .addData(iData('photo','64a','',{'varying':'4'}))
+    .addData(iData('price','12p2',''))
+    )
+   )
+  
+  # xmlservice
+  itool.call(itransport)
+  
+  # output
+  chglibl = itool.dict_out('chglibl')
+  if not 'success' in chglibl:
+    print (chglibl['error'])
+    exit()
+  
+  product = itool.dict_out('product')
+  html_result = ""
+  
+  if 'success' in product:
+    html_result = ( "<style>table, th, td {border: 1px solid black;border-collapse: collapse;}"
+       "th, td {padding: 5px;text-align: left;width: 200px;}tr:nth-child(even) {background-color: #f3f3f3;}"
+       "tr:nth-child(odd) {background-color:#fff;}th {background-color: #008ABF;color: white}</style>")
+    html_result += ('<table border="1">\n'
+                    "<th>title</th>\n"
+                    "<th>price</th>\n"
+                    "<th>photo</th>\n")
+    prods = product['findMe']
+    if 'title' in prods:
+      tmp = prods
+      prods = [tmp]
+    for r in prods:
+      if 'title' in r:
+        html_result += "<tr>"
+        html_result += "<td>" + r['title'] + "</td>"
+        html_result += "<td>" + r['price'] + "</td>"
+        html_result += "<td><img src='http://65.183.160.36/hats/master/" + r['photo'] + "'/></td>"
+        html_result += "</tr>\n"
+      else:
+        html_result += "<tr><td colspan='3'>" + str(r) + "</td></tr>\n"
+    html_result += '</table>\n'
+  else:
+    html_result += product['error']
+    html_result += str(product)
+  return html_result
+
 # ---------------------
 # command line testing only (optional)
 # ---------------------
-if not 'environ' in locals():
-  environ = dict()
-  environ['REQUEST_METHOD'] = 'GET'
-  if len(sys.argv) > 2:
-    environ['QUERY_STRING'] = '&'.join(sys.argv)
-  else:
-    print("ERROR: Command line test missing arguments.")
-    print("Example: python3 xmlhats.py myCat=1 myMax=20")
-    exit()
-else:
-  pathname, junk = os.path.split(environ['SCRIPT_NAME'])
-  student = pathname.replace("/","")
-  print("<h3>Student: " + student + "</h3>")
- 
-# ---------------------
-# remote call to yips
-# ----------------------
-itransport = iRestCall("http://65.183.160.36/cgi-bin/xmlcgi.pgm","*NONE","*NONE")
-
-# ---------------------
-# normal fastcgi processing
-# ----------------------
-if environ['REQUEST_METHOD'] == 'POST':
-  environ['QUERY_STRING'] = environ['wsgi.input'].readline().decode()
-form = cgi.parse_qs(environ['QUERY_STRING'])
-myCat = form['myCat'][0]
-myMax = form['myMax'][0]
-myCount = '0'
-findMe = ''
-
-#     D ARRAYMAX        c                   const(999)
-#     D prod_t          ds                  qualified based(Template)
-#     D  prod                         10i 0
-#     D  cat                          10i 0
-#     D  title                        64a   varying(4)
-#     D  photo                        64a   varying(4)
-#     D  price                        12p 2
-#      *+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#      * main(): Control flow
-#      *+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#     D Main            PI 
-#     D  myCat                        10i 0
-#     D  myMax                        10i 0
-#     D  myCount                      10i 0
-#     D  findMe                             likeds(prod_t) dim(ARRAYMAX)
-itool = iToolKit()
-itool.add(iCmd('chglibl', 'CHGLIBL LIBL(HATS) CURLIB(HATS)'))
-itool.add(
- iPgm('product','PRODUCT')
- .addParm(iData('myCat','10i0',myCat))
- .addParm(iData('myMax','10i0',myMax))
- .addParm(iData('myCount','10i0',myCount,{'enddo':'myCount'}))
- .addParm(
-  iDS('findMe',{'dim':'999','dou':'myCount'})
-  .addData(iData('prod','10i0',''))
-  .addData(iData('cat','10i0',''))
-  .addData(iData('title','64a','',{'varying':'4'}))
-  .addData(iData('photo','64a','',{'varying':'4'}))
-  .addData(iData('price','12p2',''))
-  )
- )
-
-# xmlservice
-itool.call(itransport)
-
-# output
-chglibl = itool.dict_out('chglibl')
-if not 'success' in chglibl:
-  print (chglibl['error'])
-  exit()
-
-product = itool.dict_out('product')
-if 'success' in product:
-  print('<table border="1">')
-  print("<th>title</th>")
-  print("<th>price</th>")
-  print("<th>photo</th>")
-  prods = product['findMe']
-  if 'title' in prods:
-    tmp = prods
-    prods = [tmp]
-  for r in prods:
-    if 'title' in r:
-      print("<tr>")
-      print("<td>" + r['title'] + "</td>")
-      print("<td>" + r['price'] + "</td>")
-      print("<td><img src='http://65.183.160.36/hats/master/" + r['photo'] + "'/></td>")
-      print("</tr>")
-    else:
-      print("<tr><td colspan='3'>" + str(r) + "</td></tr>")
-  print('</table>')
-else:
-  print (product['error'])
-  print (str(product))
+print (show_table())
