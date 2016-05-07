58cf5df8c370dcf82396e4806c4387f1123ba223
Local Linda on Orion on Windows <Local.Linda@common.org>
2016-05-07 15:15:26 -0400

    Add bottle front end to serve this table as a web app

---
+++ FrontEnd/runBottle.py
@@ -0,0 +1,19 @@
+#!/QOpenSys/usr/bin/python3
+from bottle import request, route, run
+from showTable import show_table
+from itoolkit import *
+from itoolkit.lib.ilibcall import *     #for local jobs
+ 
+ 
+port_number=80
+host_location='embox1.demos.ibm.com'
+username = 'PYDEMO'
+password = 'pydemo'
+ 
+ 
+@route('/')
+def sample():
+    return show_table() 
+ 
+
+run(host=host_location, port=port_number, debug=True) 
