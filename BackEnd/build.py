174707896a3662809f94d43d14e18a40955c32b3
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2016-08-25 16:13:26 -0400

    enhance build.py make tgtlib a parameter

--- BackEnd/build.py
+++ BackEnd/build.py
@@ -17,19 +17,25 @@ if len(sys.argv) > 1:
   tgtpgm = sys.argv[1];
 else:
   tgtpgm = 'product'
+if len(sys.argv) > 2:
+  tgtlib = sys.argv[2];
+else:
+  tgtlib = 'oriondemo'
 
 itransport = iLibCall()
 itool = iToolKit()
-itool.add(iCmd('chglibl', 'CHGLIBL LIBL(oriondemo) CURLIB(oriondemo)',{'error':'on'}))
+itool.add(iCmd('chglibl', 'CHGLIBL LIBL('+tgtlib+') CURLIB('+tgtlib+')',{'error':'on'}))
 itool.add(iCmd('crtsrcpf','CRTSRCPF QTEMP/BLDIFS RCDLEN(200)',{'error':'on'}))
 cwd = os.path.abspath(os.path.dirname(__file__))
 print (cwd)
 itool.add(iCmd('cpystmf', 'CPYFRMSTMF FROMSTMF(\''+cwd+'/'+tgtpgm+'.rpgle\') TOMBR(\'/QSYS.LIB/QTEMP.LIB/BLDIFS.FILE/'+tgtpgm+'.mbr\') MBROPT(*REPLACE)',{'error':'on'}))
-itool.add(iCmd('crtrpg',  'CRTBNDRPG '+tgtpgm+' SRCFILE(qtemp/bldifs) DBGVIEW(*SOURCE) OUTPUT(*PRINT) REPLACE(*YES)'))
+itool.add(iCmd('crtrpg',  'CRTBNDRPG '+tgtpgm+' SRCFILE(qtemp/bldifs) DBGVIEW(*SOURCE) OUTPUT(*PRINT) REPLACE(*YES)',{'error':'on'}))
 # xmlservice
 itool.call(itransport)
 
 # check for success
+check('chglibl', itool)
+check('crtsrcpf', itool)
 check('cpystmf', itool)
 check('crtrpg', itool)
 
